df0290c00bbc30098812f4b2178eda9a36700c31
Alexander Kluth <clooze@tuta.io>
2020-06-14 00:22:04 +0200

    First simple implementation of TCP forwarding
    Signed-off-by: Alexander Kluth <clooze@tuta.io>

--- lastenausgleicher.nim
+++ lastenausgleicher.nim
@@ -18,8 +18,8 @@ import
 
 type 
     LoadBalancingEndpoint = object
-        listenAddr*: string
-        listenPort*: Port
+        address*: string
+        port*: Port
 
 type
     Forwarder = object of RootObj
@@ -30,6 +30,48 @@ var
     hosts: seq[string]
     hostWithPort: seq[string]
 
+proc processClient(this: ref Forwarder, client: AsyncSocket) {.async.} =
+    let remote = newAsyncSocket(buffered=false)
+    await remote.connect(this.endpoint.address, this.endpoint.port)
+
+    proc clientHasData() {.async.} =
+        while not client.isClosed and not remote.isClosed:
+            let data = await client.recv(1024)
+            await remote.send(data)
+        client.close()
+        remote.close()
+
+    proc remoteHasData() {.async.} =
+        while not remote.isClosed and not client.isClosed:
+            let data = await remote.recv(1024)
+            await client.send(data)
+        client.close()
+        remote.close()
+
+    try:
+        asyncCheck clientHasData()
+        asyncCheck remoteHasData()
+    except:
+        echo getCurrentExceptionMsg()
+
+proc serve(this: ref Forwarder) {.async.} =
+    var server = newAsyncSocket(buffered=false)
+    server.setSockOpt(OptReuseAddr, true)
+    server.bindAddr(1337.Port, "127.0.0.1")
+    echo fmt"Started tcp server..."
+    server.listen()
+
+    while true:
+        let client = await server.accept()
+        echo "..Got connection "
+
+        asyncCheck this.processClient(client)
+
+proc newForwarder(endpoint: LoadBalancingEndpoint): ref Forwarder =
+    result = new(Forwarder)
+    result.endpoint = endpoint
+
+# main
 var p = initOptParser()
 while true:
     p.next()
@@ -53,8 +95,11 @@ for host in hosts:
     if hostWithPort.len == 1:
         hostWithPort.add("80")
 
-    loadBalancingEndpoints.add(LoadBalancingEndpoint(listenAddr: hostWithPort[0], listenPort: parseInt(hostWithPort[1]).Port))
+    loadBalancingEndpoints.add(LoadBalancingEndpoint(address: hostWithPort[0], port: parseInt(hostWithPort[1]).Port))
 
     echo "Added endpoint ", hostWithPort[0], " -> ", hostWithPort[1]
 
 
+var f = newForwarder(loadBalancingEndpoints[0])
+asyncCheck f.serve()
+runForever()
